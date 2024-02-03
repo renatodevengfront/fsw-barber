1c920651c6c811ab7c2ba16fbe2aca0d6ef531dc
Renato <renatodevengs@outlook.com>
2024-02-03 17:40:53 -0300

    feat: add initial service booking

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -24,7 +24,7 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
       },
       include: {
          services: true,
-      }
+      },
    });
 
    if (!barbershop) {
@@ -37,7 +37,7 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
 
          <div className="px-5 flex flex-col gap-4 py-6">
             {barbershop.services.map((service: { id: Key | null | undefined; }) => (
-               <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
+               <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user}/>
             ))}
          </div>
       </div>
