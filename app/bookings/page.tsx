4c48bbf2d955ef575d459fb442dc5dd71c8fc538
Renato <renatodevengs@outlook.com>
2024-02-05 23:31:42 -0300

    feat: ensure confirmation is shown on booking cancelation

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -5,8 +5,7 @@ import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma ";
 import BookingItem from "../_components/booking-item";
 import { Key } from "react";
-import { isFuture, isPast } from "date-fns";
-import { finished } from "stream";
+
 
 
 const BoogkingsPage = async () => {
@@ -48,25 +47,31 @@ const BoogkingsPage = async () => {
          <Header />
 
          <div className="px-5 py-6">
-            <h1 className="text-xl font-bokd">Agendamentos</h1>
+            <h1 className="text-xl font-bokd mb-6">Agendamentos</h1>
 
-            {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
-               <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Confirmados</h2>
+            {confirmedBookings.length > 0 && (
+               <>
+                  <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Confirmados</h2>
+
+                  <div className="flex flex-col gap-3">
+                     {confirmedBookings.map((booking: { id: Key | null | undefined; }) => (
+                        <BookingItem key={booking.id} booking={booking} />
+                     ))}
+                  </div>
+               </>
             )}
 
-            <div className="flex flex-col gap-3">
-               {confirmedBookings.map((booking: { id: Key | null | undefined; }) => (
-                  <BookingItem key={booking.id} booking={booking} />
-               ))}
-            </div>
+            {finishedBookings.length > 0 && (
+               <>
+                  <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
 
-            <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
-
-            <div className="flex flex-col gap-3">
-               {finishedBookings.map((booking: { id: Key | null | undefined; }) => (
-                  <BookingItem key={booking.id} booking={booking} />
-               ))}
-            </div>
+                  <div className="flex flex-col gap-3">
+                     {finishedBookings.map((booking: { id: Key | null | undefined; }) => (
+                        <BookingItem key={booking.id} booking={booking} />
+                     ))}
+                  </div>
+               </>
+            )}            
          </div>
       </>
    );
