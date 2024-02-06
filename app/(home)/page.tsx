bff325e60366a81ae6447741b1d40767da65a602
Renato <renatodevengs@outlook.com>
2024-02-06 19:06:22 -0300

    feat: ensure user can search for barbershops

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -16,18 +16,18 @@ export default async function Home() {
     db.barbershop.findMany({}),
     session?.user
       ? db.booking.findMany({
-        where: {
-          userId: (session.user as any).id,
-          date: {
+          where: {
+            userId: (session.user as any).id,
+            date: {
               gte: new Date(),
+            },
           },
-        },
-        include: {
-          service: true,
-          barbershop: true,
-        },
-      })
-    : Promise.resolve ([]),
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
   ]);
 
   return (
@@ -35,7 +35,10 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">{session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        <h2 className="text-xl font-bold">
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Olá! Vamos agendar um corte hoje?"}
         </h2>
         <p className="capitalize">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
@@ -51,33 +54,45 @@ export default async function Home() {
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+              Agendamentos
+            </h2>
             <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-              {confirmedBookings.map((booking: { id: Key | null | undefined; }) => (
-                <BookingItem key={booking.id} booking={booking} />
-              ))}
-            </div>          
+              {confirmedBookings.map(
+                (booking: { id: Key | null | undefined }) => (
+                  <BookingItem key={booking.id} booking={booking} />
+                )
+              )}
+            </div>
           </>
         )}
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop: { id: Key | null | undefined; }) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}          
+          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop: { id: Key | null | undefined; }) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}          
+          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
