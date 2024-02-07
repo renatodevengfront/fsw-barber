bf8f959bf8b65d8a5e831569249305a3b7bdbde2
Renato <renatodevengs@outlook.com>
2024-02-07 15:38:59 -0300

    refactor: add BookingInfo component to show booking information

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -222,19 +222,21 @@ const ServiceItem = ({
                     </div>
                   )}
 
-                  <BookingInfo
-                    booking={{
-                      barbershop: barbershop,
-                      date:
-                        date && hour
-                          ? setMinutes(
-                              setHours(date, Number(hour.split(":")[0])),
-                              Number(hour.split(":")[1])
-                            )
-                          : undefined,
-                      service: service,
-                    }}
-                  />
+                  <div className="py-6 px-5 border-t border-solid border-secondary">
+                    <BookingInfo
+                      booking={{
+                        barbershop: barbershop,
+                        date:
+                          date && hour
+                            ? setMinutes(
+                                setHours(date, Number(hour.split(":")[0])),
+                                Number(hour.split(":")[1])
+                              )
+                            : undefined,
+                        service: service,
+                      }}
+                    />
+                  </div>
 
                   <SheetFooter className="px-5">
                     <Button
